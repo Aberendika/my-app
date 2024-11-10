4e99bb869fef8384e72adb3dfaedd4664f1793de
Aleksandar Berendika <brapooo@outlook.com>
2024-11-10 08:26:58 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path:'',
+    component:HeaderComponent
+},
+{
+    path:'',
+    component:ProductsComponent
+}
+];
